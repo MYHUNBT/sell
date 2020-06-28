ab586d0712fc6992de1f47bc3d78b6e19d193782
liuboting <532747090@qq.com>
2020-06-28 08:33:16 +0800

    1

---
+++ src/main/java/com/neusoft/repository/OrderMasterRepository.java
@@ -0,0 +1,14 @@
+package com.neusoft.repository;
+
+import com.neusoft.dataobject.OrderMaster;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface OrderMasterRepository extends JpaRepository<OrderMaster,String> {
+    Page<OrderMaster> findByBuyerOpenId(String buyerOpenid, Pageable pageable);
+
+
+}
